c1d31f5be42fc9136cfdec95eec068d45afe5961
chesneysd <chesneysd@live.com>
2025-05-10 18:37:26 -0400

    Updated tests and move the wrokspace file

--- src/test_python.py
+++ src/test_python.py
@@ -1,4 +1,15 @@
-# Simple program to print numbers from 1 to 10
+#Simple program to validate the container
+
+import torch
+print("Torch version:", torch.__version__)
+print("CUDA available:", torch.cuda.is_available())
+print("CUDA version:", torch.version.cuda)
+print("Number of GPUs:", torch.cuda.device_count())
+print("Current device:", torch.cuda.current_device())
+print("Device name:", torch.cuda.get_device_name(0))
+print("Device properties:", torch.cuda.get_device_properties(0))
+print("Device count:", torch.cuda.device_count())
+print("Current device name:", torch.cuda.get_device_name(torch.cuda.current_device()))
 
 for i in range(1, 11):
     print(i)
